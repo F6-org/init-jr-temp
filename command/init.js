d60fd39fc4cc99ff99bb92d7f2864ff65a5c4bc8
谢雨琦 <yuqi@bj-m-13848a.local>
2018-12-16 19:46:27 +0800

    去掉无用模版

--- command/init.js
+++ command/init.js
@@ -1,38 +1,94 @@
 'use strict'
-const exec = require('child_process').exec
-const co = require('co')
-const prompt = require('co-prompt')
-const config = require('../templates')
-const chalk = require('chalk')
+const child_process = require('child_process');
+const exec = child_process.exec;
+const execSync = child_process.execSync;
+const co = require('co');
+const prompt = require('co-prompt');
+const fs = require('fs');
+const chalk = require('chalk');
 
 module.exports = () => {
 	co(function* () {
-		// 处理用户输入
-		let tplName = yield prompt('Template name: ')
-		let projectName = yield prompt('Project name: ')
-		let gitUrl
-		let branch
+		let projectName = yield prompt('Project name（项目名称）: ');
+		let pureTempUrl;
 
-		if (!config.tpl[tplName]) {
-			console.log(chalk.red('\n × Template does not exit!'))
-			process.exit()
-		}
-		gitUrl = config.tpl[tplName].url
-		branch = config.tpl[tplName].branch
-
+		pureTempUrl = "http://git.sc.weibo.com/horizon/pureFrame.git";
 		// git命令，远程拉取项目并自定义项目名
-		let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`
+		let cmdStr = `git clone ${pureTempUrl} ${projectName} && cd ${projectName}`;
 
-		console.log(chalk.white('\n Start generating...'))
+		console.log(chalk.white('\n Start generating...'));
 
-		exec(cmdStr, (error, stdout, stderr) => {
+		execSync(cmdStr, (error, stdout, stderr) => {
 			if (error) {
-				console.log(error)
-				process.exit()
+				console.log(error);
+				process.exit();
 			}
-			console.log(chalk.green('\n √ Generation completed!'))
-			console.log(`\n cd ${projectName} && npm install \n`)
-			process.exit()
+			console.log(chalk.green('\n √ Generation completed!!!'));
 		})
+
+		let projectOnlineUrl = yield prompt('Project online url（项目线上地址定义 例：//js.t.sinajs.cn/c2p/purchase/lucky/）: ');
+
+		function editFile(params) {
+			//现将json文件读出来
+			let fileUrl = `./${projectName}/routes/config.js`;
+			// fs.readFile(fileUrl, function (err, data) {
+			let setting =
+				`module.exports = {
+	routes: {
+		'/home': {
+			component: 'Home/Home',
+			title: '首页',
+		},
+		'/info': {
+			component: 'Info/Info',
+			title: '信息',
+		},
+		'/': {
+			component: 'Error/Error',
+			title: '出错啦',
+		}
+	},
+	baseUrl: '${projectOnlineUrl}'
+};`
+			fs.writeFileSync(fileUrl, setting);
+		}
+
+		yield editGit();
+
+		function* editGit() {
+			var gitUrl = yield prompt('git url（项目线上git地址）: ');
+			console.log(gitUrl, "0000000000000000")
+
+			if (gitUrl == '') {
+				let cmdStr = `cd ${projectName} && git init`;
+				execSync(cmdStr, (error, stdout, stderr) => {
+					if (error) {
+						console.log(error);
+						process.exit();
+					}
+					console.log(chalk.red('\n √ ------------ 要记得手动配置项目git地址哦 -------------'));
+					console.log(`\n cd ${projectName} && npm install \n`)
+					process.exit();
+				})
+			}
+
+			let cmdStr = `cd ${projectName} && git init && git remote rm origin && git remote add origin ${gitUrl} && env GIT_SSL_NO_VERIFY=true git push origin master`;
+
+			console.log(chalk.white(`\n Start git push ${gitUrl}.........................`));
+
+			exec(cmdStr, (error, stdout, stderr) => {
+				if (error) {
+					console.log(error);
+					process.exit();
+				}
+				console.log(chalk.green('\n √ git push completed!!!'));
+				console.log(`\n cd ${projectName} && npm install \n`)
+				process.exit();
+			})
+		}
+		
+		editFile();
 	})
+
+
 }
